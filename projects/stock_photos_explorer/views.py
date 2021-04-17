a0f13698b877149420d145dcfe4974229eb526e1
unknown <melvinchia623600@gmail.com>
2021-04-17 21:41:52 +0800

    update

--- projects/stock_photos_explorer/views.py
+++ projects/stock_photos_explorer/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.template import RequestContext
 from django.http import HttpResponse
+import requests
+from bs4 import BeautifulSoup as bs
+import cloudscraper
 
 def home(request):
 	return render(request, 'stock_photos_explorer/main.html', {'title': 'Stock Photos Explorer'})
@@ -8,7 +11,11 @@ def home(request):
 def pixabay_index(request):
 	if not request.COOKIES.get('api'):
 		return redirect('pixabay_ask_api')
-	return render(request, 'stock_photos_explorer/pixabay-index.html', {'title': 'Pixabay API Explorer'})
+	if request.method == 'GET':
+		query = request.GET.get('q')
+		if query: return render(request, 'stock_photos_explorer/pixabay-query.html', {'title': 'Search Result for {} - Pixabay API Explorer'.format(query).title()})
+		image = bs(cloudscraper.create_scraper().get('https://pixabay.com').content, 'lxml').find('picture').find('source')['srcset']
+		return render(request, 'stock_photos_explorer/pixabay-index.html', {'title': 'Pixabay API Explorer', 'image': image})
 
 def pixabay_ask_api(request):
 	if request.method == 'GET':
